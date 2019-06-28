b725109ac1b5998001532416be53e4293b3d1557
shawn <shawnantonucci2018@gmail.com>
2019-06-27 23:08:49 -0700

    SidePanel/ChatPanel structure added

---
+++ src/components/ChatPanel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./ChatPanel.css"
+
+const ChatPanel = () => {
+    return (
+        <div className="ChatPanel">
+            <div>Room 1</div>
+            <div>messages</div>
+            <div>send messages</div>
+        </div>
+    )
+}
+
+export default ChatPanel
---
+++ src/components/SidePanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SidePanel.css'
+
+const SidePanel = () => {
+    return (
+        <div className="SidePanel">
+            <div>Group-Up</div>
+            <div>Shawn</div>
+            <ul>
+                <li>Room 1</li>
+                <li>Room 2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default SidePanel

8fa8797430c77a63514931624381238da1c1b4ff
shawn <shawnantonucci2018@gmail.com>
2019-06-28 23:06:09 -0700

    added chat input

--- src/components/ChatPanel.js
+++ src/components/ChatPanel.js
@@ -5,8 +5,18 @@ const ChatPanel = () => {
     return (
         <div className="ChatPanel">
             <div>Room 1</div>
-            <div style={{flexGrow: 1}}>messages</div>
-            <div>send messages</div>
+            <div style={{flexGrow: 1}}>
+                <ul>
+                    <li>message 1</li>
+                    <li>message 2</li>
+                    <li>message 3</li>
+                    <li>message 4</li>
+                </ul>
+            </div>
+            <div style={{display: "flex"}}>
+                <input style={{flexGrow: 1}} placeholder="insert message ..." />
+                <button>Send Message</button>
+            </div>
         </div>
     )
 }

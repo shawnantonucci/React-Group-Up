b5f507c615b616d7ecd45b09717fd60e93d51811
shawn <shawnantonucci2018@gmail.com>
2019-06-29 19:15:52 -0700

    sending room msg to db with timestamp

--- src/components/ChatPanel/SendMessage.js
+++ src/components/ChatPanel/SendMessage.js
@@ -1,16 +1,42 @@
-import React from "react";
-import "./SendMessage.css"
+import React, { useContext, useState } from "react";
+import "./SendMessage.css";
+import UserContext from "../../context/UserContext";
+import CurrentRoomContext from "../../context/CurrentRoomContext";
+import firebase from '../../logic/firebase'
 
-const SendMessage = ({messagesRefFirebase}) => {
+const SendMessage = ({ messagesRefFirebase }) => {
+    const { user } = useContext(UserContext);
+    const { currentRoom } = useContext(CurrentRoomContext);
+    const [messageText, setMessageText] = useState("");
+
+    const createMessage = messageId => ({
+        id: messageId,
+        text: messageText,
+        roomId: currentRoom.id,
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        user
+    });
+
+    const messageId = messagesRefFirebase.push().key;
+
+    const newMessage = createMessage(messageId);
+
     return (
-        <div className="SendMessage" >
-            <input placeholder="insert message ..." />
+        <div className="SendMessage">
+            <input
+                onChange={event => setMessageText(event.target.value)}
+                placeholder="insert message ..."
+                value={messageText}
+            />
             <button
                 onClick={() =>
                     messagesRefFirebase
-                        .child("id2")
-                        .set({ text: "some text 2" })
-                        .then(msg => console.log(`set success : ${msg}`))
+                        .child(messageId)
+                        .set(newMessage)
+                        .then(msg => {
+                            console.log(`set success : ${msg}`);
+                            setMessageText("");
+                        })
                         .catch(err => console.log(`set error : ${err}`))
                 }
             >

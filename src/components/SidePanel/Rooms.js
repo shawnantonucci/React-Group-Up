5a5c1855d6aaf0b25ade55be21ba72f3a2b02ccc
shawn <shawnantonucci2018@gmail.com>
2019-06-29 20:00:44 -0700

    click on room to change current room

--- src/components/SidePanel/Rooms.js
+++ src/components/SidePanel/Rooms.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useContext } from "react";
+import CurrentRoomContext from "../../context/CurrentRoomContext";
 
 function Rooms() {
+    const { setCurrentRoom } = useContext(CurrentRoomContext);
     return (
         <div>
             <ul>
-                <li>Room 1</li>
-                <li>Room 2</li>
+                <li
+                    onClick={() => setCurrentRoom({
+                        id: "idroom1",
+                        name: "room1",
+                        description: "desc1"
+                    })}
+                >
+                    Room 1
+                </li>
+                <li
+                    onClick={() => setCurrentRoom({
+                        id: "idroom2",
+                        name: "room2",
+                        description: "desc2"
+                    })}
+                >
+                    Room 2
+                </li>
             </ul>
         </div>
-    )
+    );
 }
 
-export default Rooms
+export default Rooms;
